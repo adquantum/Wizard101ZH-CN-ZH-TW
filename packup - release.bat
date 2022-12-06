047860bdbbcdc2765b9b7f4c5ab221651c09da8f
lv-qihao <adquantum@foxmail.com>
2022-12-06 09:04:53 +0800

    打包bat修改

--- packup - release.bat
+++ packup - release.bat
@@ -1,10 +1,10 @@
 del .\Patch-Debug\Locale_English-Root.wad
 del .\Patch-Release\Locale_English-Root.wad
 cd .\Patch-Release\
-wizwad pack Locale_English-Root.wad ..\local
+wizwad pack Locale_English-Root.wad ..\Release-Half-CN
 cd ..\
 cd .\Patch-Debug\ 
-wizwad pack Locale_English-Root.wad ..\local_test
+wizwad pack Locale_English-Root.wad ..\Debug-Full-CN
 
 copy .\Locale_English-Root.wad "D:\steam\steamapps\common\Wizard101\Data\GameData\"
 @Echo Off
