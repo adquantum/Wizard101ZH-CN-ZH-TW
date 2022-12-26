ade8881e60a0ebe9d2ddd22c5b696f9864c3a698
lv-qihao <adquantum@foxmail.com>
2022-12-26 20:26:42 +0800

    格式调整和修复

--- packup - release.bat
+++ packup - release.bat
@@ -7,21 +7,3 @@ cd .\Patch-Debug\
 wizwad pack Locale_English-Root.wad ..\Debug-Full-CN
 
 copy .\Locale_English-Root.wad "D:\steam\steamapps\common\Wizard101\Data\GameData\"
-@Echo Off
-
-Echo open 101.43.174.221 21 >ftp.up
-
-Echo blaze>>ftp.up
-
-Echo Lyuu0226@>>ftp.up
-
-
-Echo binary>>ftp.up
-
-Echo put "D:\wizproject\wiz汉化文件\Wizard101ZH-CN-ZH-TW\\Patch-Release\Locale_English-Root.wad">>ftp.up
-
-Echo bye>>ftp.up
-
-FTP -s:ftp.up
-
-del ftp.up /q
